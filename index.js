391e9830d3fac283fb3179ed1dde909672c4c89a
ecl1pzee <235312011+ecl1pzee@users.noreply.github.com>
2026-08-26 21:37:23 +0200

    No longer a base

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+function GoToProjects() {
+    window.location.replace("http://ecl1pzee.github.io/projects");
+    console.log("should have redirected now");
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// HEY YOU! YES YOU!!11!!1!!1 Use npx tsc before commiting, thanks.
+
+function GoToProjects() {
+    window.location.replace("http://ecl1pzee.github.io/projects");
+    console.log("[i] shouldve redirected now");
+}
