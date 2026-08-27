8042c796891072106cdacc88bcb02fca19480e8d
ecl1pzee <235312011+ecl1pzee@users.noreply.github.com>
2026-08-27 19:19:31 +0200

    Added GitHub redirect button

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 // HEY YOU! YES YOU!!11!!1!!1 Use npx tsc before commiting, thanks.
 
 function GoToProjects() {
-    window.location.replace("http://ecl1pzee.github.io/projects");
+    window.location.replace("https://ecl1pzee.github.io/projects");
+    console.log("[i] shouldve redirected now");
+}
+
+function GoToGithub() {
+    window.location.replace("https://github.com/ecl1pzee");
     console.log("[i] shouldve redirected now");
 }
